f714199df6527850e8b16129e260910d91914b71
Leandro Zanol <lzanol@gmail.com>
2018-06-18 18:53:40 -0300

    editing

--- src/editor/index.js
+++ src/editor/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import { parse } from 'subtitle';
 import Item from './item';
 
@@ -9,12 +8,24 @@ import './style.scss';
 export default class CaptionEditor extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			editingIndex: -1
+		};
+	}
+
+	onEditItem = item => {
+		this.setState({ editingIndex: item.index });
+	}
+
+	onSubmitItem = item => {
+		this.setState({ editingIndex: -1 });
 	}
 
 	render() {
-		const items = typeof this.props.data === 'string' ?
-			parse(this.props.data).slice(0, 1).map((v, i) =>
-				<Item key={i} value={v.text} />) : null;
+		const items = parse(this.props.data).slice(0, 10).map((v, i) =>
+			<Item key={i} index={i} data={v} editing={this.state.editingIndex === i}
+				onEdit={this.onEditItem} onSubmit={this.onSubmitItem} />);
 
 		return <ul className="caption-editor">{items}</ul>;
 	}
--- src/editor/item.js
+++ src/editor/item.js
@@ -1,47 +1,73 @@
 /* eslint-disable no-unused-vars */
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 
 export default class Item extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			isEditing: false,
-			value: props.value || ''
+			...props.data
 		};
+
+		this.index = props.index;
 	}
 
-	onLabelClick = () => {
-		this.setState({ isEditing: true });
+	msToTime(ms) {
+		const pad = n => `0${n}`.slice(-2);
+		const s = ms / 1000 | 0,
+			m = (s / 60 | 0) % 60,
+			h = (m / 60 | 0) % 60;
+
+		return `${pad(h)}:${pad(m)}:${pad(s % 60)}`;
 	}
 
-	onTfLoad = ref => {
+	update() {
+		if (this.element)
+			this.element.innerHTML = this.state.text.replace(/\n/g, '<br>');
+	}
+
+	onTextClick = () => {
+		this.props.editing || this.props.onEdit(this);
+	}
+
+	submit = cancel => {
+		cancel || this.setState({ text: this.element.innerHTML });
+		this.props.onSubmit();
+	}
+
+	onLoad = ref => {
+		this.element = ref;
+
 		if (ref == null)
 			return;
 
-		ref.innerHTML = this.state.value.replace(/\n/g, '<br>');
+		this.update();
 		ref.focus();
 	}
 
 	onKeyDown = e => {
 		const cancel = e.keyCode === 27;
 
-		if (e.keyCode === 13 && !e.shiftKey || cancel) {
-			this.setState({
-				isEditing: false,
-				value: cancel ? this.state.value : e.target.innerHTML
-			});
-		}
+		if (e.keyCode === 13 && !e.shiftKey || cancel)
+			this.submit(cancel);
 	}
 
-	onBlur = e => {
-		this.setState({ isEditing: false });
+	componentDidUpdate() {
+		this.update();
 	}
 
 	render() {
-		return <li><div ref={this.onTfLoad}
-			contentEditable={this.state.isEditing} onMouseDown={this.onLabelClick}
-			onKeyDown={this.onKeyDown} onBlur={this.onBlur} /></li>;
+		const submit = this.props.editing ? <div className="actions">
+			<button onClick={() => this.submit(true)}>Cancel</button>
+			<button onClick={() => this.submit()}>Ok</button>
+		</div> : null;
+
+		return <li>
+			<div className="time"><div>{this.msToTime(this.state.start)}</div>
+				<div>{this.msToTime(this.state.end)}</div></div>
+			<div className="text" ref={this.onLoad} contentEditable={this.props.editing}
+				onMouseDown={this.onTextClick} onKeyDown={this.onKeyDown} />
+			{submit}
+		</li>;
 	}
 }
